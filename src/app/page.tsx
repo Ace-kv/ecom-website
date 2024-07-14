878a9620e377ce6ae775a75dcdbf500749fc6554
Ace-kv <talhashahid100@yahoo.com>
2024-07-14 10:12:13 +0500

    changed the product card component product in app/page.tsx to even the size difference with the other two - prop product={data[2]} -> product={data[4]}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,7 +136,7 @@ const Home = () => {
           <div className="product-showcase">
             <ProductCard product={data[0]} />
             <ProductCard product={data[1]} />
-            <ProductCard product={data[2]} />
+            <ProductCard product={data[4]} />
           </div>
         </section>
 
