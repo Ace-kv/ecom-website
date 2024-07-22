95b21b2b8e6b3950bbb1558802d166dbf57fd72a
Ace-kv <talhashahid100@yahoo.com>
2024-07-22 11:45:58 +0500

    did proper error handling for when the fetch response was no ok to avoid using map method over undefined and crashing the site

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -17,13 +17,41 @@ export interface DResult {
 const allProductData: SProduct[] = await getAllProductData()
 
 const Cart = async () => {
-    let user_id = cookies().get('user_id')?.value
-
-    if (!user_id) user_id = undefined
-
+    const user_id = cookies().get('user_id')?.value
+    
     const res = await fetch(`https://ecom-website-amber.vercel.app/api/cart?user_id=${user_id}`, {
         method: 'GET',
-    }).then(response => response.json())
+        cache: 'no-store'
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch request:', error);
+        return null;
+    });
+
+    if (!res || !Array.isArray(res.res)) {
+        console.error('Invalid response format:', res);
+        return (
+            <>
+                <ToastErrorAlert text='Your Cart is Empty' errId='empty-cart-err' />
+                <div className="cart-wrapper">
+                    <h2>Shopping Cart</h2>
+                    <div className="cart-container">
+                        <div className="cart-items">
+                            <div className="empty-cart">
+                                <ShoppingBag size={130} strokeWidth={1.5}/>
+                                <h1>Your shopping bag is empty</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     const product_ids: string[] = res.res.map((product: DResult) => product.product_id)
 
@@ -36,12 +64,12 @@ const Cart = async () => {
 
     return (
         <>    
-            { !res && <ToastErrorAlert text='Your Cart is Empty' errId='empty-cart-err' />}
+            { cartProducts.length === 0 && <ToastErrorAlert text='Your Cart is Empty' errId='empty-cart-err' />}
             <div className="cart-wrapper">
                 <h2>Shopping Cart</h2>
                 <div className="cart-container">
                     <div className="cart-items">
-                        {!res && !user_id ? 
+                        {cartProducts.length === 0 ? 
                             <div className="empty-cart">
                                 <ShoppingBag size={130} strokeWidth={1.5}/>
                                 <h1>Your shopping bag is empty</h1>
